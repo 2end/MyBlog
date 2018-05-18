b66603bad5f1816609b8b305fb50c56aae58146d
Roman Myshchyshyn <38398964+2end@users.noreply.github.com>
2018-05-18 13:50:47 +0300

    https

--- MyBlog/Startup.cs
+++ MyBlog/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -30,7 +31,10 @@ namespace MyBlog
             services.AddIdentity<User, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationContext>()
                 .AddDefaultTokenProviders();
-            services.AddMvc();
+            services.AddMvc(options =>
+			{
+				options.Filters.Add(new RequireHttpsAttribute());
+			});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
